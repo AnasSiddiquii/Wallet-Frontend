44c85e7511db390dbeb1c488852bd21e0171f1f6
AnasSiddiquii <sidanas1anassid.com>
2023-03-07 01:10:40 +0530

    uploaded

---
+++ src/components/User/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div className='container mb-5 body'>
+      <h2 className='text-primary mt-4'>Cart</h2>
+    </div>
+  )
+}
+
+export default Cart
---
+++ src/components/User/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <div className='container mb-5 body'>
+      <h2 className='text-primary mt-4'>Orders</h2>
+    </div>
+  )
+}
+
+export default Orders
